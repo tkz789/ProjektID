2bbd379f235b1df59d95f4850cc4d53b519bc3db
Dawid Zapolski <dawid.zapolski@student.uj.edu.pl>
2024-06-01 16:28:09 +0200

    Fix database name from system

--- basic.py
+++ basic.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template, request, redirect, url_for
 import psycopg2
 import sys
+import os
 
 app = Flask(__name__)
 
 def get_db_connection():
     conn = psycopg2.connect(
-        database="dawid",
-        user="dawid"
+        database=os.getlogin(),
+        user=os.getlogin()
     )
     return conn
 
